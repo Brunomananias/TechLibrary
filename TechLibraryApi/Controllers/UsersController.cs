9bd1b1d7e6b20535a08a3f30a9893927d4f674fb
Bruno Ananias <brunomananias2009@hotmail.com>
2025-02-19 13:55:18 -0300

    Aula1 - Fluxo de Registro de Usuario

---
+++ TechLibraryApi/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using TechLibrary.Communication.Requests;
+using TechLibrary.Communication.Responses;
+using TechLibrary.Exception;
+using TechLibraryApi.UseCases.Users.Register;
+
+namespace TechLibraryApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status400BadRequest)]
+        public IActionResult Create(RequestUserJson request) 
+        {
+            try
+            {
+                RegisterUserUseCase useCase = new RegisterUserUseCase();
+                var response = useCase.Execute(request);
+                return Created(string.Empty, response);
+            }
+            catch(TechLibraryException ex)
+            {
+                return BadRequest(new ResponseErrorMessagesJson
+                {
+                    Errors = ex.GetErrorMessages()
+                });
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseErrorMessagesJson
+                {
+                    Errors = ["Erro Desconhecido"]
+                });
+            }
+            
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Created();
+        }
+    }
+}
---
+++ TechLibraryApi/Infrastructure/TechLibraryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechLibraryApi.Domain.Entities;
+namespace TechLibraryApi.Infrastructure
+
+{
+    public class TechLibraryDbContext : DbContext
+    {       
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=C:\\Users\\Bruno Ananias\\Downloads\\TechLibraryDb.db");
+        }
+    }
+}
